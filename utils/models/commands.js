8009c901c291a95d730d50311d6f08df73cead19
ImyerDev <netbios1@gmail.com>
2020-12-08 20:10:07 -0300

    Delete commands.js

--- utils/models/commands.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require("mongoose")
-let CommandSchema = new mongoose.Schema({
-guildID: { type: String },
-commandsDisable: { type: Array },
-commandsEnable: { type: Array }
-})
-let Commands = module.exports = mongoose.model("Commands", CommandSchema)
