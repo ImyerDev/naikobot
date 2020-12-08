4632328bf447ec2c6ff51e560a1c76dddea16a04
ImyerDev <netbios1@gmail.com>
2020-12-08 20:11:10 -0300

    Update prefix.js

--- utils/functions/prefix.js
+++ utils/functions/prefix.js
@@ -1,6 +1,6 @@
 class PrefixManager{
 constructor() {
-	this.prefix = async function Prefix(client, guildid){
+	this.currentPrefix = async function Prefix(client, guildid){
 	 let x = await client.prefixes.findOne({guildID: guildid});
 	 if(x) {
 	 return x.prefix
@@ -9,25 +9,30 @@ constructor() {
      return '-'
 	 }
 	}
-	this.updatePrefix = async function UpdatePrefix(message, client, guildid, newprefix) {
+	this.deletePrefix = async function UpdatePrefix(client, guildid) {
 	let x = await client.prefixes.findOne({guildID: guildid})
         if(x == null) {
-        return await client.prefixes.create({
-	guildID: guildid,
-        addedBy: message.author.id,
-	updateBy: message.author.id,
-	prefixAdded: Date.now(),
-	prefix: newprefix
-	})
+        await client.prefixes.deleteMany({guildID: guildid})
         }
-        await client.prefixes.deleteMany({guildID: guildid}, {prefix: newprefix})
-        return newprefix
+        return true
         }
-	this.hasPrefix = async function RealPrefix(client, guildid) {
+	this.setPrefix = async function RealPrefix(client, guildid, newprefix) {
 	 let x = await client.prefixes.findOne({guildID: guildid})
-	 if(x) return true
-	 if(!x) return false
-	}
+	 if(x == null) {
+         const nuller = new client.prefixes({
+         guildID: guildid,
+         prefix: newprefix
+         })
+         nuller.save();
+         } else {
+         await client.prefixes.deleteMany({guildID: guildid});
+         const New = new client.prefixes({
+         guildID: guildid,
+         prefix: newprefix
+         })
+         New.save();
+         }
+        }
 }
 }
 
